5162707732e32e1c07498b599c3877c0b6d6a501
marekrum <marek111@mail.ua>
2019-04-19 09:42:16 +0300

    Changed the HTML for the site.

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def post_list(request):
+    return render(request, 'catalog/post_list.html', {})
